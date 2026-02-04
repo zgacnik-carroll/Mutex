54032944958af8df96156edea4a958e5dd9b00f0
Zack Gacnik <zgacnik@carroll.edu>
2026-02-03 20:58:26 -0700

    Working Mutex file

--- src/Mutex.java
+++ src/Mutex.java
@@ -1,10 +1,16 @@
 public final class Mutex {
 
+    private volatile boolean locked = false;
+
     public synchronized boolean acquire() {
-        return true;
+        if (!locked) {
+            locked = true;
+            return true;
+        }
+        return false;
     }
 
     public synchronized void release() {
-        return;
+        locked = false;
     }
 }
